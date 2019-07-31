879428ac04513afe0f203ee8de3516efe774f249
Krzysztof Stempinski <Krzysztof.Stempinski@externals.adidas.com>
2019-07-31 16:01:09 +0200

    Work on testing in AppVeyor etc

--- test-runner/test-runner-script.ps1
+++ test-runner/test-runner-script.ps1
@@ -1,3 +1,13 @@
 $testCountFailed = 0
 
+$testName = "test A"
+$frameworkName = "jsc-test-runner"
+$jscBinaryName = "jsc.exe"
+$outcome = "Passed"
+
+Add-AppveyorTest    -Name $testName 
+                    -Framework $frameworkName -
+                    -Filename $jscBinaryName 
+                    -Outcome $outcome 
+
 exit testCountFailed
